3810b201a1cf8792620804147d4293b44992a91d
chiiicken <65784006+chiiicken5538@users.noreply.github.com>
2022-07-29 13:11:17 +0200

    Create codeDatabase.lua

---
+++ storage/codeDatabase.lua
@@ -0,0 +1,103 @@
+getgenv().____IIlllIllIlllIll = {
+
+    abcdefghijklmnop = {
+
+        a = {
+            ___a_value_ = nil
+        },
+        b = {
+            ___b_value_ = nil
+        },
+        c = {
+            ___c_value_ = nil
+        },
+        d = {
+            ___d_value_ = nil
+        },
+        e = {
+            ___e_value_ = nil
+        },
+        f = {
+            ___f_value_ = nil
+        },
+        g = {
+            ___g_value_ = nil
+        },
+        h = {
+            ___h_value_ = nil
+        },
+        i = {
+            ___i_value_ = nil
+        },
+        j = {
+            ___j_value_ = nil
+        },
+        k = {
+            ___k_value_ = nil
+        },
+        l = {
+            ___l_value_ = nil
+        },
+        m = {
+            ___m_value_ = nil
+        },
+        n = {
+            ___n_value_ = nil
+        },
+        o = {
+            ___o_value_ = nil
+        },
+        p = {
+            ___p_value_ = nil
+        },
+    },
+
+    a = {
+        ___a_value_ = nil
+    },
+    b = {
+        ___b_value_ = nil
+    },
+    c = {
+        ___c_value_ = nil
+    },
+    d = {
+        ___d_value_ = nil
+    },
+    e = {
+        ___e_value_ = nil
+    },
+    f = {
+        ___f_value_ = nil
+    },
+    g = {
+        ___g_value_ = nil
+    },
+    h = {
+        ___h_value_ = nil
+    },
+    i = {
+        ___i_value_ = nil
+    },
+    j = {
+        ___j_value_ = nil
+    },
+    k = {
+        ___k_value_ = nil
+    },
+    l = {
+        ___l_value_ = nil
+    },
+    m = {
+        ___m_value_ = nil
+    },
+    n = {
+        ___n_value_ = nil
+    },
+    o = {
+        ___o_value_ = nil
+    },
+    p = {
+        ___p_value_ = nil
+    },
+}
